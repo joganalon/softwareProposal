2c461ce47e7ab20cdc0d039a4cf1f83fc5db064f
iamjonel7 <iamjonel7@gmail.com>
2024-05-12 22:34:30 +0800

    modified:   venv/ANCprj/core/__pycache__/urls.cpython-311.pyc 	modified:   venv/ANCprj/core/urls.py 	modified:   venv/ANCprj/db.sqlite3 	modified:   venv/ANCprj/templates/userauths/sign-up.html 	modified:   venv/ANCprj/userauths/__pycache__/views.cpython-311.pyc 	modified:   venv/ANCprj/userauths/views.py

--- venv/ANCprj/userauths/views.py
+++ venv/ANCprj/userauths/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+
 
 def RegisterView(request):
 
-    if request.method == 'post':
-        form = UserRegisterForm(request.post)
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
+            print('successful')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hey {username}, Your account was created succesfully')
+            new_user = authenticate(username = form.cleaned_data['email'], #same with .cleaned_data.get('email')
+                                    password = form.cleaned_data['password1'] #password1 == password authenticate field
+                                    ) 
+            login(request, new_user)
+            return redirect('core:index') #return to index.html in core app
 
     else:
         form = UserRegisterForm()
+        print('unsuccessful')
 
     context = {
         'form': form,
