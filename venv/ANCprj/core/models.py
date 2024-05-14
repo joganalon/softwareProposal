311ee74916e3ae1d882befe17e43368b3873ec96
iamjonel7 <iamjonel7@gmail.com>
2024-05-14 12:37:31 +0800

    modified:   venv/ANCprj/core/__pycache__/models.cpython-311.pyc 	modified:   venv/ANCprj/core/models.py     new files:  Pillow, shortuuidfield

--- venv/ANCprj/core/models.py
+++ venv/ANCprj/core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+from django.utils.html import mark_safe
+from userauths.models import User
 
-# Create your models here.
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Category(models.Model):
+    cid = ShortUUIDField(unique=True, length=10, max_length=20, prefix='cat', alphabet='abcdefgh12345')
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='category')
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def category_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+    
+    def __str__(self):
+        return self.title
+    
+
+class Vendor(models.Model):
+    vid = ShortUUIDField(unique=True, length=10, max_length=20, prefix='ven', alphabet='abcdefgh12345')
+
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to=user_directory_path)
+    description = models.TextField(null=True, blank=True)
+
+    address = models.CharField(max_length=100, default='Ateneo')
+    contact = models.CharField(max_length=100, default='09091234567')
+    chat_resp_time = models.CharField(max_length=100, default='100')
+    authentic_rating = models.CharField(max_length=100, default='100')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
